ce93afa8ca48498190591ead6f089646712011c8
Ashadi Fajarrohman <56154632+Ashadi-fajarrohman@users.noreply.github.com>
2021-01-03 11:21:23 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import bs4
+import requests
+
+url = 'https://jadwalsholat.pkpu.or.id/?id=36'
+contents = requests.get(url)
+
+response = bs4.BeautifulSoup(contents.text, 'html.parser')
+data = response.find_all('tr', 'table_highlight')
+data = data[0]
+
+sholat = {}
+i = 0
+for d in data:
+    if i == 1:
+        sholat['subuh'] = d.get_text()
+    elif i == 2:
+        sholat['zuhur'] = d.get_text()
+    elif i == 3:
+        sholat['ashar'] = d.get_text()
+    elif i == 4:
+        sholat['maghrib'] = d.get_text()
+    elif i == 5:
+        sholat['isya'] = d.get_text()
+    i += 1
+
+print(sholat)
